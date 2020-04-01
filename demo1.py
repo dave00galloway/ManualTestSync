797931217a8e2aa6a8a694618779a8779d82ae3b
david galloway <david.galloway@yahoo.co.uk>
2020-04-01 20:56:22 +0100

    master -  big refactor

--- demo1.py
+++ demo1.py
@@ -1,80 +1,108 @@
+import json
 import os
 
 import requests
 from requests import Response
 
+PUBLIC_TESTPAD_URL = 'https://ontestpad.com/login'
+
+
+class User(object):
+    USER = None
+
+    @classmethod
+    def get(cls):
+        return cls.USER
+
+    @classmethod
+    def set(cls, value):
+        cls.USER = value
+
+
+class AuthenticationData(object):
+    def __init__(self, csrf_token=None, session_id=None, username=None, password=None, testpad_url=None, **kwargs):
+        if username is None:
+            username = os.getenv('username')
+        if password is None:
+            password = os.getenv('password')
+        if testpad_url is None:
+            testpad_url = os.getenv('testpadurl')
+        self.username = username
+        self.csrf_token = csrf_token
+        self.session_id = session_id
+        self.password = password
+        self.testpad_url = testpad_url
+        self.kwargs = kwargs
+
 
 def main():
-    csrf_token = None
-    session_id = None
-    a = requests.get('https://ontestpad.com/login')
-    print(a.status_code)
-    print(a.cookies.items())
-
-    csrf_token, session_id = update_cookies(a, csrf_token, session_id)
-    update_cookies()
-
-    # csrftoken1 = "ylQA0LzyufPrOGC0slEMbPKhaiQVf5y9"
-    # sessionid1 = "n5b65ddshtoroxwhedbva3va22tidwmq"
-    headers2 = {
-        "x-csrftoken": csrf_token,
-        "referer": "https://ontestpad.com/login",
-        'cookie': "csrftoken={csrftoken}; sessionid={sessionid};".format(csrftoken=csrf_token, sessionid=session_id)
-    }
-    b = requests.post(
-        'https://ontestpad.com/login',
-        headers=headers2,
-        json='{"data":null}')
-    print(b.status_code)
-    print(b.cookies.items())
-
-    csrftoken2 = "ylQA0LzyufPrOGC0slEMbPKhaiQVf5y9"
-    sessionid2 = "n5b65ddshtoroxwhedbva3va22tidwmq"
-    headers3 = {
-        "x-csrftoken": csrftoken2,
-        "referer": "https://ontestpad.com/login",
-        'cookie': "csrftoken={csrftoken}; sessionid={sessionid};".format(csrftoken=csrftoken2, sessionid=sessionid2)
-    }
-    c = requests.post(
-        'https://ontestpad.com/login',
-        headers=headers3,
-        json='{"data":null}')
-    print(c.status_code)
-    print(c.cookies.items())
-
-    csrftoken3 = os.getenv('csrftoken')
-    sessionid3 = os.getenv('sessionid')
-    testpadurl = os.getenv('testpadurl')
     project = os.getenv('project')
     targetfolder = os.getenv('targetfolder')
+    User.set(authenticate())
+
+    scripts = load_scripts(user=User.get(), project=project, targetfolder=targetfolder)
+    assert scripts
+    print(json.dumps(scripts, indent=4))
+
+
+def load_scripts(user=None, project=None, targetfolder=None):
     headers = {
-        'x-csrftoken': csrftoken3,
-        'referer': "{url}/{project}/{folder}".format(url=testpadurl, project=project, folder=targetfolder),
-        'cookie': "csrftoken={csrftoken}; sessionid={sessionid};".format(csrftoken=csrftoken3, sessionid=sessionid3)
+        'x-csrftoken': user.csrf_token,
+        'referer': "{url}/{project}/{folder}".format(url=user.testpad_url, project=project, folder=targetfolder),
+        'cookie': "csrftoken={csrftoken}; sessionid={sessionid};".format(csrftoken=user.csrf_token,
+                                                                         sessionid=user.session_id)
     }
-    d = requests.post(
-        '{url}/a/{project}/{folder}/loadScripts'.format(url=testpadurl, project=project, folder=targetfolder),
+    load_scripts_response = requests.post(
+        '{url}/a/{project}/{folder}/loadScripts'.format(url=user.testpad_url, project=project, folder=targetfolder),
         headers=headers, json='{"data":null}')
-    print(d.status_code)
+    user = update_cookies(response=load_scripts_response, user=user)
+    user.kwargs = {"new": "entry"}
+    return load_scripts_response.json()
 
 
-def update_cookies(response=None, csrf_token=None, session_id=None):
+def authenticate(csrf_token=None, session_id=None, username=None, password=None, testpad_url=None, **kwargs):
+    _user = AuthenticationData(csrf_token=csrf_token, session_id=session_id, username=username, password=password,
+                               testpad_url=testpad_url, kwargs=kwargs)
+    public_login_response = requests.get(PUBLIC_TESTPAD_URL)
+    _user = update_cookies(response=public_login_response, user=_user)
+    headers = {
+        "x-csrftoken": csrf_token,
+        "referer": PUBLIC_TESTPAD_URL,
+        'cookie': "csrftoken={csrftoken}; sessionid={sessionid};".format(csrftoken=_user.csrf_token,
+                                                                         sessionid=_user.session_id)
+    }
+    form_data = {
+        "csrfmiddlewaretoken": _user.csrf_token,
+        "email": _user.username,
+        "password": _user.password,
+        "js": "y",
+        "next": None
+    }
+    private_login_response = requests.post(PUBLIC_TESTPAD_URL, headers=headers, data=form_data)
+    _user = update_cookies(response=private_login_response, user=_user)
+    return _user
+
+
+def update_cookies(response=None, user=None, **kwargs):
     if response is None:
         response = Response()
         if response.status_code is None:
             raise ValueError("response cannot be None local variables = {locals}".format(locals=locals()))
+    if user is None:
+        user = AuthenticationData(**kwargs)
     _csrf_token, _session_id = None, None
 
     for cookie in response.cookies:
         if cookie.name == "csrftoken":
             _csrf_token = cookie.value
+            user.csrf_token = _csrf_token
         if cookie.name == "sessionid":
             _session_id = cookie.value
-        if csrf_token and session_id:
+            user.session_id = _session_id
+        if _csrf_token and _session_id:
             break
 
-    csrf_token, session_id = _csrf_token, _session_id
-    return csrf_token, session_id
+    return user
 
 
 if __name__ == '__main__':
