870f2f59671c7ccffaab83316a1cbd54f9854b89
david galloway <david.galloway@yahoo.co.uk>
2020-05-11 15:49:09 +0100

    master -  rename script done, but headers need trimming

--- testpad/folders.py
+++ testpad/folders.py
@@ -10,8 +10,37 @@ def create_folder(user=User.get(), project=Project.get(), targetfolder=None, nam
             "user, project, targetfolder and name must be specified. {locals}".format(locals=str(locals())))
 
     new_folder = _create_new_folder(user=user, project=project, parent=targetfolder, **kwargs)
+    folder_id = new_folder["data"]["id"]
+
+    new_folder = _rename_folder(user=User.get(), project=Project.get(), parent=targetfolder, folder_id=folder_id,
+                                name=name,
+                                **kwargs)
 
     return new_folder
+
+
+def _rename_folder(user=User.get(), project=Project.get(), folder_id=None, name=None, **kwargs):
+    headers = {
+        'x-csrftoken': user.csrf_token,
+        'x-requested-with': 'XMLHttpRequest',
+        'referer': "{url}/project/{project}/".format(url=user.testpad_url, project=project),
+        'content-type': 'application/json',
+        'sec-fetch-site': 'same-origin',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-dest': 'empty',
+        'accept-language': 'en-US,en;q=0.9',
+        'accept': 'application/json, text/javascript, */*; q=0.01',
+        'Accept-Encoding': 'deflate, gzip',
+        'cookie': "csrftoken={csrftoken}; sessionid={sessionid};".format(csrftoken=user.csrf_token,
+                                                                         sessionid=user.session_id)
+    }
+    rename_folder_response = requests.post(
+        '{url}/a/project/{project}/folder/{folder}/name'.format(url=user.testpad_url, project=project,
+                                                                folder=folder_id),
+        headers=headers, json={"data": name})
+    assert rename_folder_response.status_code is 200
+    authentication.update_cookies(response=rename_folder_response, user=user, **kwargs)
+    return rename_folder_response.json()
 
 
 def _create_new_folder(user=User.get(), project=Project.get(), parent=None, **kwargs):
@@ -40,6 +69,6 @@ if __name__ == '__main__':
     Project.set(os.getenv('project'))
     User.set(authentication.authenticate())
 
-    folder = create_folder(user=User.get(), project=Project.get(), targetfolder=target_folder, name="test_me")
+    folder = create_folder(user=User.get(), project=Project.get(), targetfolder='f5', name="test_me")
     assert folder
     print(json.dumps(folder, indent=4))
