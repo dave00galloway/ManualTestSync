ba77fa512c3041f567f5899c205885cd400f42c7
david galloway <david.galloway@yahoo.co.uk>
2020-05-19 16:48:11 +0100

    master -  tidy up main method

--- main.py
+++ main.py
@@ -1,30 +1,30 @@
-import json
 import os
 
-from test_loader import file_system_parser
-from testpad import authentication, scripts, statics
+from test_loader import testpad_loader
+from testpad import authentication
 from testpad.statics import User, Project
 
 PUBLIC_TESTPAD_URL = 'https://ontestpad.com/login'
 
 
 def main():
+    # todo: add argparse to override the env vars which should only be a fallback
+    # todo: add logging
+    # todo: add unit tests
+    # todo: add error handling
     project = os.getenv('project')
-    targetfolder = os.getenv('targetfolder')
+    # targetfolder = os.getenv('targetfolder')
     checkout_dir = os.getenv('checkoutDir')
-    Project.set(project)
-    User.set(authentication.authenticate())
-    project, user = statics.set_project_user(project=project, user=User.get())
-
-    loaded = scripts.load_scripts(user=user, project=project, targetfolder=targetfolder)
-    assert loaded
-    print(json.dumps(loaded, indent=4))
     if checkout_dir.startswith("~"):
         home = os.path.expanduser("~")
         checkout_dir = os.path.join(home, checkout_dir.replace("~/", '', 1))
-    l = file_system_parser.list_files(dir_=checkout_dir)
-    kv = file_system_parser.structure_files(file_tuples=l)
-    print(json.dumps(kv, indent=4))
+
+    Project.set(project)
+    User.set(authentication.authenticate())
+
+    ldr = testpad_loader.Loader(user=User.get(), project=Project.get(),  # targetfolder=targetfolder,
+                                checkout_dir=checkout_dir)  # , create_folder_func=mock_create_folder)
+    ldr.load_tests()
 
 
 if __name__ == '__main__':
